cfe6cfc045ad26e28179cf1db7480d304adfabbf
eunjin0212 <eunjin0212@gmail.com>
2023-03-13 23:14:17 +0900

    ✨ 끝말잇기 게임

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,39 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/word-chain-game">끝맛잇기 게임</RouterLink>
+    </nav>
   </header>
-
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default defineComponent({
+  components: { RouterView },
+  setup() {
+    return {}
+  }
+})
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
+<style lang="scss">
+  body {
+    display: flex;
+    flex-direction: column;
+    padding: 40px 20px;
+  }
   header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    left: 0;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/views/WordChainGame.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>2인이서 하는 끝말잇기 게임</p>
+    <div class="input-box">
+      player 1 : <input type="text" :value="player1" />
+      <br />
+      player 2 : <input type="text" :value="player2" />
+    </div>
+    <div>
+      <b>이전 글자 : </b>
+      <span>{{ lastWord }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'WordChainGame',
+  setup() {
+    const player1 = ref('')
+    const player2 = ref('')
+    const lastWord = ref('')
+    return {
+      player1,
+      player2,
+      lastWord
+    }
+  }
+})
+</script>
+
+<style lang="scss"></style>
