aabe05765dfc0e03c94ac491fbe428f29b525d29
eunjin0212 <eunjin0212@gmail.com>
2023-04-17 23:28:43 +0900

    💄사다리 스타일 추가

--- src/views/LadderGame.vue
+++ src/views/LadderGame.vue
@@ -19,7 +19,7 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <div v-if="!dialog" class="d-flex flex-column ladder-wrapper">
+    <div v-if="!dialog" class="d-flex flex-column ladder-wrapper justify-space-between">
       <div class="d-flex justify-space-between text-center" style="gap: 8px">
         <v-sheet v-for="(item) in itemCount" :key="item" :style="`width: calc(100vw / ${itemCount})`">{{ item }}</v-sheet>
       </div>
@@ -27,7 +27,6 @@
         <v-text-field v-for="(item, idx) in itemCount" :key="item" v-model="items[idx]"></v-text-field>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -53,5 +52,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-.ladder-wrapper {}
+.ladder-wrapper {
+  height: 90vh;
+}
 </style>
